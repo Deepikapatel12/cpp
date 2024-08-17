de22f34e46acf9347b5c23c4bb9450827be1e505
Deepikapatel <deepikapatel5687@gmail.com>
2024-08-17 22:58:55 +0530

    is prime

---
+++ is_prime.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+int a,b;
+void Addition()
+{
+    a+b;
+}
+void sub()
+{
+    a-b;
+}
+void divid()
+{
+ a/b;
+}
+int main()
+{
+    /*int num1, num2;
+    cout<<"Enter any two numbers\n";
+    cin>>num1>>num2;*/
+    Addition(5,3);
+}
